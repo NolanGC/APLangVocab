c1843733ee81180b326833305ba23ef4880c1dde
NolanGC <nolangclement@gmail.com>
2019-08-26 22:24:13 -0700

    Update autoQuizlet.py

--- autoQuizlet.py
+++ autoQuizlet.py
@@ -2,13 +2,24 @@
 quizlet = ''
 #Words in the unit
 words = 20 
+
+def empty(l):
+    empty = True
+    for i in range(len(l)):
+        if(l[i] != '\n'):
+            empty = False
+    return empty
+def num_caps(l):
+    return sum(1 for c in l if c.isupper())
+
 with open('raw.txt', 'r', encoding='utf-8') as raw:
     lines = raw.readlines()
 #print(lines)
-for i in range(0,4*words - 20,4):
-    quizlet += lines[i].rstrip()
-    quizlet += '|'
-    quizlet += lines[i+1]
-    quizlet += '\n'
+with open("out.txt", "w+") as f:
+    for i in range(len(lines)):
+        if(lines[i].count(' ') <= 2 and not empty(lines[i]) and num_caps(lines[i]) == 1):
+            quizlet += lines[i].replace('\n','').replace(' ','')
+            quizlet += '|' + lines[i+1]
+    f.write(quizlet)
 
-print(quizlet)
+
